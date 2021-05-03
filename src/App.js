c167d385937e49607b50e237cc5b8b305ee1745f
mukul98s <mymukul112@gmail.com>
2021-05-03 22:49:57 +0530

    Basic setup of app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import Header from "./components/Header";
+import useFetch from "./utils/useFetch";
+
 function App() {
-  return <div className="App"></div>;
+  const { data, isLoading, error } = useFetch(
+    "https://api.covid19api.com/summary"
+  );
+
+  console.log(data);
+
+  return (
+    <div className="App">
+      <div className="title">
+        <div className="title-container section">
+          <h1>
+            <span>
+              <i className="fas fa-viruses"></i>
+            </span>
+            Covid Tracker
+          </h1>
+        </div>
+      </div>
+      <Header data={data} isLoading={isLoading} error={error} />
+    </div>
+  );
 }
 
 export default App;
